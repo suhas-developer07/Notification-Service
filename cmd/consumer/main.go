ef2af2837afe021783f95acf39de8c60fd294e54
suhas-developer07 <sharmasuhas22@gmail.com>
2025-08-10 19:24:03 +0530

    sqs working

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/suhas-developer07/notification-service/internal/queue"
+)
+
+func main() {
+	ctx := context.Background()
+
+	sqsClient, err := queue.NewSQSClient(ctx, "http://localhost:4566/000000000000/notify")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msgs, err := sqsClient.ReceiveMessage(ctx, 5)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, msg := range msgs {
+		fmt.Println("Recieved:", msg)
+	}
+}
